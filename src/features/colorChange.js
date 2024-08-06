d54eb9bcba9b8141cb16f988865f7d0082938030
net11FellaJ <julian.fella@neteleven.de>
2024-08-06 12:17:56 +0200

    refresh if all lazy images are loaded

--- src/features/colorChange.js
+++ src/features/colorChange.js
@@ -85,8 +85,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 const lazyImages = Array.from(document.querySelectorAll("img[loading='lazy']"));
-lazyImages.forEach(function (lazyImage) {
-  lazyImage.addEventListener("load", function () {
-    ScrollTrigger.refresh();
-  });
+
+Promise.all(
+  lazyImages.map(function (lazyImage) {
+    return new Promise(function (resolve) {
+      lazyImage.addEventListener("load", resolve);
+    });
+  })
+).then(function () {
+  ScrollTrigger.refresh();
 });
