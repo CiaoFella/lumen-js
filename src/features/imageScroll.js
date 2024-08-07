694dc54f0ec66c7852b36a5b7b547339f5d0ec49
net11FellaJ <julian.fella@neteleven.de>
2024-08-07 23:47:39 +0200

    change to css animation and intersection observer

--- src/features/imageScroll.js
+++ src/features/imageScroll.js
@@ -2,23 +2,21 @@ window.addEventListener("DOMContentLoaded", () => {
   const section = document.querySelector("[data-scroll-image=section]");
   const imageWraps = section.querySelectorAll("[data-scroll-image=wrap]");
 
-  imageWraps.forEach((wrap) => {
-    const image = wrap.querySelector("[data-scroll-image=image]");
-    const scale = image.dataset.scale || 1.17;
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: wrap,
-        start: "top bottom",
-        end: "bottom center",
-        scrub: true,
-      },
-      defaults: {
-        duration: 1,
-      },
-    });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const image = entry.target.querySelector("[data-scroll-image=image]");
+          const scale = image.dataset.scale || 1.17;
+          image.style.setProperty("--scale", scale);
+          image.classList.add("animate");
+        }
+      });
+    },
+    {
+      threshold: 0.1, // Adjust this value as needed
+    }
+  );
 
-    tl.from(image, {
-      scale: scale,
-    });
-  });
+  imageWraps.forEach((wrap) => observer.observe(wrap));
 });
