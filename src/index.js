6fe4032709d2e3265da17b31481f85d38b63679a
net11FellaJ <julian.fella@neteleven.de>
2024-08-07 19:42:52 +0200

    add image scroll

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import './features/barHover.js';
-import './features/videoPlayer.js';
-import './features/textScroll.js';
-import './features/cycleElements.js'
-import './features/menu.js';
-import './features/colorChange.js';
-import './features/preloader.js';
-import './features/textScroll.js';
+import "./features/barHover.js";
+import "./features/videoPlayer.js";
+import "./features/textScroll.js";
+import "./features/imageScroll.js";
+import "./features/cycleElements.js";
+import "./features/menu.js";
+import "./features/colorChange.js";
+import "./features/preloader.js";
+import "./features/textScroll.js";
