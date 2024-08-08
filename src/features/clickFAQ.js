a842f48851529963c717cda489c12478153a1413
net11FellaJ <julian.fella@neteleven.de>
2024-08-08 22:34:12 +0200

    adapt timing

--- src/features/clickFAQ.js
+++ src/features/clickFAQ.js
@@ -7,25 +7,15 @@ window.addEventListener("DOMContentLoaded", () => {
     const icon = faqItem.querySelector("[data-faq=icon]");
 
     const clickTl = gsap.timeline({
-      defaults: { duration: 0.5 },
+      defaults: { duration: 0.25 },
       paused: true,
     });
 
     gsap.set(faqAnswer, { height: 0 });
 
     clickTl
-      .to(faqAnswer, { height: "auto", duration: 0.5 })
-      .to(icon, { rotate: 45 }, "<")
-      .to(
-        faqQuestion,
-        { color: "white", duration: 0.25, ase: "power2.inOut" },
-        "<"
-      )
-      .to(
-        faqQuestion,
-        { padding: "0 16px", duration: 0.25, ase: "power2.inOut" },
-        "<"
-      );
+      .to(faqAnswer, { height: "auto", ease: "none" })
+      .to(icon, { rotate: 45 }, "<");
 
     faqQuestion.addEventListener("click", () => {
       faqQuestion.classList.toggle("is-active");
