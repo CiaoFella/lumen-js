b88de0889d1a2520f6a5d83726c5d5857859f4d7
net11FellaJ <julian.fella@neteleven.de>
2024-08-08 11:49:25 +0200

    add error handling

--- src/features/imageScroll.js
+++ src/features/imageScroll.js
@@ -1,30 +1,35 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const section = document.querySelector("[data-scroll-image=section]");
-  const imageWraps = section.querySelectorAll("[data-scroll-image=wrap]");
+  const sections = document.querySelectorAll("[data-scroll-image=section]");
 
-  imageWraps.forEach((wrap) => {
-    const image = wrap.querySelector("[data-scroll-image=image]");
-    const scale = image.dataset.scale || 1.17;
+  if (!sections || sections.length === 0) return;
+  sections.forEach((section) => {
+    const imageWraps = section.querySelectorAll("[data-scroll-image=wrap]");
 
-    // Add will-change property to the image element
-    image.style.willChange = "transform";
+    if (!imageWraps || imageWraps.length === 0) return;
+    imageWraps.forEach((wrap) => {
+      const image = wrap.querySelector("[data-scroll-image=image]");
+      const scale = image.dataset.scale || 1.17;
 
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: wrap,
-        start: "top bottom",
-        end: "bottom center",
-        scrub: true,
-      },
-      defaults: {
-        duration: 1,
-      },
-    });
+      // Add will-change property to the image element
+      image.style.willChange = "transform";
 
-    tl.from(image, {
-      scale: 1.2,
-      force3D: true, // Forces 3D acceleration
-      ease: "none",
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: wrap,
+          start: "top bottom",
+          end: "bottom center",
+          scrub: true,
+        },
+        defaults: {
+          duration: 1,
+        },
+      });
+
+      tl.from(image, {
+        scale: scale,
+        force3D: true, // Forces 3D acceleration
+        ease: "none",
+      });
     });
   });
 });
